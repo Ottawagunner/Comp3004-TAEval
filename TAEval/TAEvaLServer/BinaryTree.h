7b8944349d22cd85c86cc4c83d3e7ebd306fe0fc
Draymire <zachstoner@gmail.com>
2013-10-28 19:20:22 -0400

    BinaryTree Should be all like awesome and stuff now. Disclaimer: I take no responsibility should it explode in our faces violently.

--- TAEval/TAEvaLServer/BinaryTree.h
+++ TAEval/TAEvaLServer/BinaryTree.h
@@ -97,6 +97,14 @@ private:
 	void decrementSize();
 	void setSize(int);
 
+	void balance();
+	void balance(Node*);
+	bool isBalanced() const;
+	void ReorganizePatternOne(Node*);
+	void ReorganizePatternTwo(Node*);
+	void ReorganizePatternThree(Node*);
+	void ReorganizePatternFour(Node*);
+
 	Node* root;
 	int numberOfNodes;
 };
@@ -172,7 +180,9 @@ void BinaryTree<KEY,VALUE>::add(KEY* addKey, VALUE* addValue){
 				}
 			}
 		}
+		balance();
 	}
+
 }
 
 template <class KEY, class VALUE>
@@ -262,6 +272,7 @@ void BinaryTree<KEY,VALUE>::remove(KEY* locatorKey){
 
 	remove(findNode(locatorKey));
 	decrementSize();
+	balance();
 }
 
 template <class KEY, class VALUE>
@@ -403,10 +414,8 @@ void BinaryTree<KEY,VALUE>::remove(Node* theNode){
 
 			fixDepth(theNode->rightChild);
 
-			VALUE* backup = theNode->rightChild->data;
 			delete(theNode);
 
-			Node* temp = findNode(backup);
 		}
 	} else if(theNode->leftChild != LEAF){//Change
 		if (theNode->leftChild->rightChild != LEAF){
@@ -551,6 +560,247 @@ void BinaryTree<KEY,VALUE>::setSize(int input){
 //Comment: Contemplate passing a reference rather then 
 //			an actual value.
 	numberOfNodes = input;
+}
+
+
+//////Balance
+
+template <class KEY, class VALUE>
+void BinaryTree<KEY,VALUE>::balance(){
+//
+//     Function : balance
+//
+// Input Params : None
+//
+//Output Params : None
+//
+//  Description : Private Helper Function
+//					Optimizes the Tree
+//
+	if (!isBalanced()){
+		balance(root);
+	}
+}
+
+template <class KEY, class VALUE>
+bool BinaryTree<KEY,VALUE>::isBalanced() const{
+//
+//     Function : isBalanced
+//
+// Input Params : None
+//
+//Output Params : True if Balanced, False if not
+//
+//  Description : Private Helper Function
+//					Checks to see if the Tree is balanced and Optimal
+//
+	int leftDepth, rightDepth, middleDepth;
+	if (root->leftChild != LEAF)
+		leftDepth = root->leftChild->depth;
+	 else
+		leftDepth = 0;
+
+	if (root->rightChild != LEAF)
+		rightDepth = root->rightChild->depth;
+	else
+		rightDepth = 0;
+
+	middleDepth = root->depth;
+
+	// (1<<middleDepth) is a left Shift being applied to one to calulate the 
+	// Minimum number of nodes for that depth to exist assuming optimal distribution.
+	if (numberOfNodes >= (1<<middleDepth) && (leftDepth == rightDepth))
+		return true;
+	else 
+		return false;
+}
+
+template <class KEY, class VALUE>
+void BinaryTree<KEY,VALUE>::balance(Node* theNode){
+//
+//     Function : balance
+//
+// Input Params : Node to start the balancing act at
+//
+//Output Params : None
+//
+//  Description : Private Helper Function
+//					Actually Does the Optimization through recursive calls
+//
+	if (theNode->leftChild != LEAF && theNode->rightChild != LEAF){
+		if (theNode->leftChild->depth == theNode->rightChild->depth){
+			balance(theNode->leftChild);
+			balance(theNode->rightChild);
+		}
+	} else if (theNode->leftChild != LEAF){
+		if (theNode->leftChild->leftChild != LEAF || theNode->leftChild->rightChild != LEAF){
+			if (theNode->leftChild->leftChild != LEAF && (theNode->leftChild->leftChild->leftChild != LEAF && theNode->leftChild->leftChild->rightChild != LEAF)){
+				ReorganizePatternOne(theNode);
+				if (theNode->parent != ROOT) balance(theNode->parent);
+			} else if (theNode->leftChild->rightChild != LEAF && (theNode->leftChild->rightChild->leftChild != LEAF && theNode->leftChild->rightChild->rightChild != LEAF)){
+				ReorganizePatternTwo(theNode);
+				if (theNode->parent != ROOT) balance(theNode->parent);
+			}
+		}
+	} else if (theNode->rightChild != LEAF){
+		if (theNode->rightChild->leftChild != LEAF || theNode->rightChild->rightChild != LEAF){
+			if (theNode->rightChild->leftChild != LEAF && (theNode->rightChild->leftChild->leftChild != LEAF && theNode->rightChild->leftChild->rightChild != LEAF)){
+				ReorganizePatternFour(theNode);
+				if (theNode->parent != ROOT) balance(theNode->parent);
+			} else if (theNode->rightChild->rightChild != LEAF && (theNode->rightChild->rightChild->leftChild != LEAF && theNode->rightChild->rightChild->rightChild != LEAF)){
+				ReorganizePatternThree(theNode);
+				if (theNode->parent != ROOT) balance(theNode->parent);
+			}
+		}
+	}
+}
+
+template <class KEY, class VALUE>
+void BinaryTree<KEY,VALUE>::ReorganizePatternOne(Node* theNode){
+	//Reorganize Pattern 1
+	// Given a Tree with the Order (50,25,10,5,15,30,55) which has 4 levels
+	// This rearranges it into (25,10,5,15,50,30,55) which has 3 levels
+	// Order is chosen traveling left down the tree and picking
+	// Nodes the first time you come across them. 
+	Node* tempNode = theNode->leftChild;
+	tempNode->parent = theNode->parent;
+
+	//Relink the 'root' parent/child relationship
+	if (theNode->parent != ROOT && theNode->parent->leftChild == theNode){
+		theNode->parent->leftChild = tempNode;
+	} else if (theNode->parent != ROOT){
+		theNode->parent->rightChild = tempNode;
+	}
+
+	//Relink the leftChild parent/child relationship
+	theNode->leftChild = tempNode->rightChild;
+	tempNode->rightChild->parent = theNode;
+
+	//Relink the rightChild parent/child relationship
+	theNode->parent = tempNode;
+	tempNode->rightChild = theNode;
+
+	if (theNode->rightChild != LEAF){
+		fixDepth(theNode->rightChild);
+	} else {
+		theNode->depth = 0;
+		fixDepth(theNode);
+	}
+}
+
+template <class KEY, class VALUE>
+void BinaryTree<KEY,VALUE>::ReorganizePatternTwo(Node* theNode){
+	//Reorganize Pattern 2
+	//Given a Tree with the Order (50,25,10,40,30,45,55) which has 4 levels
+	//This rearranges it into (40,25,10,30,50,45,55) which has 3 levels
+	//Order is chosen the same as in Pattern 1
+	Node* tempNode = theNode->leftChild->rightChild;
+	tempNode->parent = theNode->parent;
+	
+	if (theNode->parent != ROOT){
+		if (theNode->parent->leftChild == theNode){
+			theNode->parent->leftChild = tempNode;
+		} else {
+			theNode->parent->rightChild = tempNode;
+		}
+	}
+
+	theNode->leftChild->parent = tempNode;
+	theNode->leftChild->rightChild = tempNode->leftChild;
+
+	if (tempNode->leftChild != LEAF){
+		tempNode->leftChild->parent = theNode->leftChild;
+	}
+
+	tempNode->leftChild = theNode->leftChild;
+	theNode->parent = tempNode;
+	theNode->leftChild = tempNode->rightChild;
+	tempNode->rightChild->parent = theNode;
+	tempNode->rightChild = theNode;
+
+	if (theNode->rightChild != LEAF) fixDepth(theNode->rightChild);
+	else if (theNode->leftChild != LEAF) fixDepth(theNode->leftChild);
+	else{
+		theNode->depth = 0;
+		fixDepth(theNode);
+	}
+	
+	if (tempNode->leftChild->leftChild != LEAF) fixDepth(tempNode->leftChild->leftChild);
+	else if (tempNode->leftChild->rightChild != LEAF) fixDepth(tempNode->rightChild->leftChild);
+	else{
+		tempNode->leftChild->depth = 0;
+		fixDepth(tempNode->leftChild);
+	}		
+}
+
+template <class KEY, class VALUE>
+void BinaryTree<KEY,VALUE>::ReorganizePatternThree(Node* theNode){
+	//Reorganize Pattern 3
+	//Given a Tree with the Order (50,25,75,60,100,80,125) which has 4 levels
+	//This rearranges it into (75,50,25,60,100,80,125) which has 3 levels
+	//Order is chosen the same as in Pattern 1
+	Node* tempNode = theNode->rightChild;
+	tempNode->parent = theNode->parent;
+
+	if (theNode->parent != ROOT && theNode->parent->leftChild == theNode){
+		theNode->parent->leftChild = tempNode;
+	} else if (theNode->parent != ROOT){
+		theNode->parent->rightChild = tempNode;
+	}
+
+	theNode->rightChild = tempNode->leftChild;
+	tempNode->leftChild->parent = theNode;
+
+	theNode->parent = tempNode;
+	tempNode->leftChild = theNode;
+
+	if (theNode->leftChild != LEAF){
+		fixDepth(theNode->leftChild);
+	} else {
+		theNode->depth = 0;
+		fixDepth(theNode);
+	}
+}
+
+template <class KEY, class VALUE>
+void BinaryTree<KEY,VALUE>::ReorganizePatternFour(Node* theNode){
+	//Reorganize Pattern 4
+	//Given a Tree with the Order (50,25,100,75,60,80,125) which has 4 levels
+	//This rearranges it into (75,50,25,60,100,80,125) which has 3 levels
+	//Order is chosen the same as in Pattern 1	
+	Node* tempNode = theNode->rightChild->leftChild;
+	tempNode->parent = theNode->parent;
+	
+	if (theNode->parent != ROOT){
+		if (theNode->parent->leftChild == theNode){
+			theNode->parent->leftChild = tempNode;
+		} else {
+			theNode->parent->rightChild = tempNode;
+		}
+	}
+
+	theNode->rightChild->parent = tempNode;
+	theNode->rightChild->leftChild = tempNode->leftChild;
+	tempNode->leftChild->parent = theNode->rightChild;
+	tempNode->leftChild = theNode->rightChild;
+	theNode->parent = tempNode;
+	theNode->rightChild = tempNode->rightChild;
+	tempNode->rightChild->parent = theNode;
+	tempNode->rightChild = theNode;
+
+	if (theNode->rightChild != LEAF) fixDepth(theNode->rightChild);
+	else if (theNode->leftChild != LEAF) fixDepth(theNode->leftChild);
+	else{
+		theNode->depth = 0;
+		fixDepth(theNode);
+	}
+	
+	if (tempNode->leftChild->leftChild != LEAF) fixDepth(tempNode->leftChild->leftChild);
+	else if (tempNode->leftChild->rightChild != LEAF) fixDepth(tempNode->rightChild->leftChild);
+	else{
+		tempNode->leftChild->depth = 0;
+		fixDepth(tempNode->leftChild);
+	}
 }
 
 
